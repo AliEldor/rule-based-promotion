03cd5959a472625219318794f277e51246416a02
Ali Eldor <alieldor02@gmail.com>
2025-07-02 19:06:19 +0300

    add products table migration

--- backend/database/migrations/2025_07_02_160131_create_products_table.php
+++ backend/database/migrations/2025_07_02_160131_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 150);
+            $table->foreignId('category_id')->nullable()->constrained('categories')->onDelete('set null');
+            $table->decimal('unit_price', 10, 2);
             $table->timestamps();
         });
     }
