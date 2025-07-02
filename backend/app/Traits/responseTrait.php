ef3f6e52184eca60c4c274ad1af6e4babfb8fbd3
Ali Eldor <alieldor02@gmail.com>
2025-07-03 02:19:51 +0300

    add responseTrait

--- backend/app/Traits/responseTrait.php
+++ backend/app/Traits/responseTrait.php
@@ -2,7 +2,42 @@
 
 namespace App\Traits;
 
-trait responseTrait
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+trait ResponseTrait
 {
-    //
+    public function successResponse($data, $message = null, $statusCode = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data
+        ], $statusCode);
+    }
+
+    public function errorResponse($message, $statusCode = 400, $errors = null)
+    {
+        $response = [
+            'success' => false,
+            'message' => $message
+        ];
+        
+        if ($errors) {
+            $response['errors'] = $errors;
+        }
+        
+        return response()->json($response, $statusCode);
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            $this->errorResponse(
+                'Validation failed',
+                422,
+                $validator->errors()
+            )
+        );
+    }
 }
