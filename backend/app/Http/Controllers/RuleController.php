6d97d963b8c66f9fa6e875cb31c828a071a481c7
Ali Eldor <alieldor02@gmail.com>
2025-07-04 00:07:45 +0300

    initialize RuleController class

---
+++ backend/app/Http/Controllers/RuleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateRuleRequest;
+use App\Http\Requests\UpdateRuleRequest;
+use App\Services\RuleService;
+use App\Traits\ResponseTrait;
+
+class RuleController extends Controller
+{
+    use ResponseTrait;
+
+    
+}
