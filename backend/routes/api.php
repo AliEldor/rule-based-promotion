b4e71ba9aabc1e8046cc113e645cb9269773e7fb
Ali Eldor <alieldor02@gmail.com>
2025-07-04 00:38:11 +0300

    add rule and evaluation endpoints

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RuleController;
+use App\Http\Controllers\EvaluateController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+// Rule CRUD endpoint
+Route::apiResource('rules', RuleController::class);
+
+// Rule evaluation endpoint
+Route::post('/evaluate', [EvaluateController::class, 'evaluate']);
